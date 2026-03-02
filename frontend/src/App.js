cd134915148c467d4e83c06268a95bd121505e4b
sayuwanjj <teawithlemon.97@gmail.com>
2026-03-02 15:20:46 +0300

    master: сохранение

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import UsersPage from "./pages/UsersPage/UsersPage";
+
+function App() {
+    return (
+        <div className="App">
+            <UsersPage />
+        </div>
+    );
+}
+
+export default App;
